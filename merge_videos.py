ffb1fc8a304bc09cb3e5655b7abf48b001ad5869
He Linming <hlm52pk@163.com>
2018-04-25 16:11:49 +0800

    Update merge_videos.py
    automation

--- merge_videos.py
+++ merge_videos.py
@@ -1,15 +1,30 @@
-# Created by Heer
+# Created by MrHeer
 # Make sure you had install ffmpeg
 # Please move it to the dir of your videos
 # Modify this program and make it suit you
 # Run it
 
 import os
+import re
+
+# get some info
+file_list = []
+extension = ''
+ext = input("Enter the extension(default -- .ts): ")
+if ext.strip() == '':
+    extension = '.ts'
+else:
+    extension = ext
+
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if re.search(extension + "$", file):
+            file_list.append(file)
 
 # create filelist.txt
-file = open('./filelist.txt', 'w')
-for i in range(1, 9):
-    file.write('file \'' + str(i) + '\'\n')
+file = open('./file_list.txt', 'w')
+for file_name in file_list:
+    file.write('file \'' + file_name + '\'\n')
     pass
 file.close()
 
@@ -17,4 +32,4 @@ file.close()
 os.system('ffmpeg -f concat -i filelist.txt -c copy output.mp4')
 
 # delete filelist.txt
-os.remove('./filelist.txt')
+os.remove('./file_list.txt')
