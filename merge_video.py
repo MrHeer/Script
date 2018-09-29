d2658149fabf4025e3b723f824dcec4472a59f6f
Linming He <hlm52pk@163.com>
2018-09-29 20:26:30 +0800

    Update and rename merge_videos.py to merge_video.py
    fix a bug

--- merge_video.py
+++ merge_video.py
@@ -1,6 +1,6 @@
 # Created by MrHeer
 # Make sure you had install ffmpeg
-# Please move it to the dir of your videos
+# Please move it to the dir of your video
 # Modify this program and make it suit you
 # Run it
 
@@ -19,17 +19,18 @@ else:
 for root, dirs, files in os.walk("."):
     for file in files:
         if re.search(extension + "$", file):
-            file_list.append(file)
+            file_list.append(int(file[0: -len(extension)]))
 
-# create filelist.txt
+# create file_list.txt
+file_list.sort()
 file = open('./file_list.txt', 'w')
 for file_name in file_list:
-    file.write('file \'' + file_name + '\'\n')
+    file.write('file \'' + str(file_name) + extension + '\'\n')
     pass
 file.close()
 
-# merge videos
-os.system('ffmpeg -f concat -i filelist.txt -c copy output.mp4')
+# merge video
+os.system('ffmpeg -f concat -i file_list.txt -c copy output.mp4')
 
-# delete filelist.txt
+# delete video
 os.remove('./file_list.txt')
