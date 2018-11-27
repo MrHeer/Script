346ada380fa0506e1c57c1708a49108b8c70f38a
MrHeer <hlm52pk@163.com>
2018-11-27 23:28:32 +0800

    modified:   rebattery.sh

--- rebattery.sh
+++ rebattery.sh
@@ -4,10 +4,17 @@ RED='\033[1m\033[31m'
 GREEN='\033[1m\033[32m'
 RES='\033[0m'
 
-info=$(cat /sys/class/power_supply/BAT1/capacity)
+capacity_info=$(cat /sys/class/power_supply/BAT1/capacity)
+status_info=$(cat /sys/class/power_supply/BAT1/status)
 
-if (( $info >= 60 )) ; then
-    echo -e "${GREEN}Remaining battery: ${info}%${RES}"
+if [[ $status_info == 'Charging' ]] ; then
+    echo -e "${GREEN}Remaining battery: ${capacity_info}%, ${status_info}${RES}"
 else
-    echo -e "${RED}Remaining battery: ${info}%${RES}"
+    if (( $capacity_info >= 30 )) ; then
+        echo -e "Remaining battery: ${capacity_info}%, ${status_info}"
+    else
+        echo -e "${RED}Remaining battery: ${capacity_info}%, ${status_info}${RES}"
+    fi
 fi
+
+
